358db1528bd1f17e92d0a3746db5da1053bf4602
TeemuKoivisto <tkoivisto456@gmail.com>
2016-03-21 15:02:13 +0200

    Fix index.js

--- index.js
+++ index.js
@@ -18,8 +18,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(require("webpack-hot-middleware")(compiler));
 }
 
-// app.use(express.static(__dirname + '/public/'));
-app.use(express.static(path.join(__dirname + '/dist/')));
+app.use(express.static(__dirname + '/public/'));
 
 // app.get("*", (req, res) => {
   // res.sendFile(path.join(__dirname, ["pubindex.html"]));
