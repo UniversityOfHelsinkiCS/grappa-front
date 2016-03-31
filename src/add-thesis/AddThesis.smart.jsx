bb201a2c00bf670a98f373295f9a3de7cd35b235
xvax <jesse.vaisanen@cs.helsinki.fi>
2016-03-31 13:39:46 +0300

    Modify add thesis react component

--- src/add-thesis/AddThesis.smart.jsx
+++ src/add-thesis/AddThesis.smart.jsx
@@ -1,38 +1,60 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+
 import { addThesis } from "../thesis/Thesis.actions";
 
-export class addThesis extends Component {
+export class ThesisCreate extends Component {
 
   constructor() {
       super();
       this.handleSubmit = this.handleSubmit.bind(this);
+      this.handleChange = this.handleChange.bind(this);
+      this.getInitialState = this.getInitialState.bind(this);
+  }
+
+  getInitialState() {
+     return {value: "asdf"};
+   }
+
+handleChange(event) {
+    event.preventDefault();
+    this.setState({value: event.target.value});
   }
 
   handleSubmit(event) {
     event.preventDefault();
     const newThesis = {
       id: this.props.id + 1,
-      author: this.props.author,
-      email: this.props.email,
-      title: this.props.title,
-      instructor: this.props.instructor,
-      urkund: this.props.urkund,
-      ethesis: this.props.ethesis,
-      abstract: this.props.abstract,
-      field: this.field.field,
-      grade: this.props.grade,
-      deadline: this.props.deadline,
+      author: fname,
+      email: email,
+      title: title,
+      instructor: instructor,
+      urkund: urkund,
+      ethesis: ethesis,
+      abstract: abstract,
+      field: field,
+      grade: grade,
+      deadline: "10.10.2010",
     };
     addThesis(newThesis);
   }
 
   render() {
+    let fname = "asd";
+    let lname = "";
+    let email = "";
+    let title = "";
+    let instructor = "";
+    let urkund = "";
+    let ethesis = "";
+    let abstract = "";
+    let field = "";
+    let grade = "";
+    let deadline = "";
 
     return (
       <div>
         <body>
-
           <header>
             <h1 id="sign">Sign in as USER</h1>
             <div id="nav" className="ui vertical pointing menu">
@@ -47,24 +69,24 @@ export class addThesis extends Component {
               <div className="field">
                 <div className="two fields">
                   <div className="field">
-                    <input type="text" name="madeby[first-name]" onChange={this.handleSubmit} placeholder="First Name"/>
+                    <input type="text" name="madeby[first-name]" value={this.state.value} onChange={this.handleChange} placeholder="First Name"/>
                   </div>
                   <div className="field">
-                    <input type="text" name="madeby[last-name]" placeholder="Last Name"/>
+                    <input type="text" name="madeby[last-name]" value={lname} placeholder="Last Name"/>
                   </div>
                 </div>
               </div>
 
               <div className="field">
                 <div className="ten wide field">
-                  <input type="text" name="madeby[email]" placeholder="Email Address"/>
+                  <input type="text" name="madeby[email]" value={email} placeholder="Email Address"/>
                 </div>
               </div>
               <h4 className="ui dividing header">Thesis Information</h4>
               <div className="field">
                 <div className="three fields">
                   <div className="field">
-                    <input type="text" name="thesis[title]" placeholder="Title"/>
+                    <input type="text" name="thesis[title]" value={title} placeholder="Title"/>
                   </div>
                   <div className="three wide field">
                     <div className="field">
@@ -95,17 +117,17 @@ export class addThesis extends Component {
               </div>
               <div className="two fields">
                 <div className="field">
-                  <input type="text" name="thesis[ethesis]" placeholder="Link to E-thesis"/>
+                  <input type="text" name="thesis[ethesis]" value={ethesis} placeholder="Link to E-thesis"/>
                 </div>
                 <div className="field">
-                  <input type="text" name="thesis[urkund]" placeholder="Link to Urkund"/>
+                  <input type="text" name="thesis[urkund]" value={urkund} placeholder="Link to Urkund"/>
                 </div>
               </div>
               <h4 className="ui dividing header">Instructors</h4>
               <div className="field">
                 <div className="three fields">
                   <div className="field">
-                    <input type="text" name="1instructor[name]" placeholder="Name"/>
+                    <input type="text" name="1instructor[name]" value={instructor} placeholder="Name"/>
                   </div>
                   <div className="four wide field">
                     <div className="field">
@@ -146,7 +168,7 @@ export class addThesis extends Component {
               <h4 className="ui dividing header">Choose the date for the Department
               Council meeting</h4>
             </form>
-            {/*<button className="ui primary button" onClick={handleSubmit}>Submit</button>*/}
+            <button className="ui primary button" onClick={this.handleSubmit}>Submit</button>
             <button className="ui primary button">Cancel</button>
           </main>
         </body>
@@ -161,4 +183,4 @@ const mapDispatchToProps = (dispatch) => ({
   },
 });
 
-export default connect(null, mapDispatchToProps)(addThesis);
+export default connect(null, mapDispatchToProps)(ThesisCreate);
