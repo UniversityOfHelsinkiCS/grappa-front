36c3a1b1f73ab9a28f5de7012a9b439fabaa25ca
Mikael Wide <mikael.wide@helsinki.fi>
2016-04-01 14:03:34 +0300

    Created actions and reducer for adding councilmeetings

--- src/councilmeeting/Councilmeeting.actions.js
+++ src/councilmeeting/Councilmeeting.actions.js
@@ -6,15 +6,14 @@ export const COUNCILMEETING_GET_ALL_FAILURE = "COUNCILMEETING_GET_ALL_FAILURE";
 export const COUNCILMEETING_SAVE_ONE_SUCCESS = "COUNCILMEETING_SAVE_ONE_SUCCESS";
 export const COUNCILMEETING_SAVE_ONE_FAILURE = "CONCILMEETING_SAVE_ONE_FAILURE";
 
-
 export const addCouncilmeeting = (Councilmeeting) => {
-	console.log("addCouncilmeeting-action called :)");
-	return {
-		type: CALL_API,
-		success: COUNCILMEETING_SAVE_ONE_SUCCESS,
-		failure: COUNCILMEETING_SAVE_ONE_FAILURE,
-		method: "post",
-		url: "/councilmeetings",
-		data: { councilmeeting: Councilmeeting },
-	};
+  console.log("addCouncilmeeting-action called :)");
+  return {
+    type: CALL_API,
+    success: COUNCILMEETING_SAVE_ONE_SUCCESS,
+    failure: COUNCILMEETING_SAVE_ONE_FAILURE,
+    method: "post",
+    url: "/councilmeetings",
+    data: Councilmeeting,
+  };
 };
