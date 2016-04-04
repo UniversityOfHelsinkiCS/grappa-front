225752b6ca527263b0c21d68fd0a0d26c30ada00
Mikael Wide <mikael.wide@helsinki.fi>
2016-04-04 14:48:09 +0300

    Init with adding dates

---
+++ src/councilmeeting/CouncilmeetingCreate.smart.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import { addCouncilmeeting } from "./councilmeeting.actions";
+
+var DatePicker = require('react-datepicker');
+var moment = require('moment');
+
+export class CouncilmeetingCreate extends Component {
+  render(){
+    return (
+
+      <div>
+      <body>
+      <header>
+      <h1>MIÄÄÄÄÄÄÄÄDDÄDDDLDLDLD!</h1>
+      </header>
+      </body>
+      </div>
+
+      )
+  }
+
+};
+
--- src/routes.jsx
+++ src/routes.jsx
@@ -3,11 +3,13 @@ import { Route, IndexRedirect, IndexRoute } from "react-router";
 import App from "./app/App.smart";
 import ThesisList from "./thesis/ThesisList.smart";
 import ThesisShow from "./thesis/ThesisShow.smart";
+import CouncilmeetingCreate from "./councilmeeting/CouncilmeetingCreate.smart";
 
 export default (
   <Route path="/" component={App}>
     <Route path="theses" component={ThesisList} />
     <Route path="theses/:id" component={ThesisShow} />
+    <Route path="councilmeeting/new" component={CouncilmeetingCreate} />
     <IndexRedirect to="/theses" />
   </Route>
 );
