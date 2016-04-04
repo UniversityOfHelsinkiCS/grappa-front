5f90b023da7a7fe6bed239439471a4970d83b17d
Mikael Wide <mikael.wide@helsinki.fi>
2016-04-04 12:54:41 +0300

    Tests working and fixed lint

--- src/councilmeeting/Councilmeeting.reducer.js
+++ src/councilmeeting/Councilmeeting.reducer.js
@@ -11,28 +11,28 @@ import {
 
 const INITIAL_STATE = fromJS({
   councilmeetinglist: [
-  {
-    id: 1,
-    date: "date1",
-  },
-  {
-    id: 2,
-    date: "date2",
-  },
-  {
-    id: 3,
-    date: "date3",
-  },
+    {
+      id: 1,
+      date: "date1",
+    },
+    {
+      id: 2,
+      date: "date2",
+    },
+    {
+      id: 3,
+      date: "date3",
+    },
   ],
 });
 
 export default function (state = INITIAL_STATE, action) {
   switch (action.type) {
     case COUNCILMEETING_SAVE_ONE_SUCCESS:
-    return state.updateIn(["councilmeetinglist"], list => list.concat(fromJS(action.payload)));    
+      return state.updateIn(["councilmeetinglist"], list => list.concat(fromJS(action.payload)));
     case COUNCILMEETING_GET_ALL_SUCCESS:
-    return state.merge().councilmeetinglist.push(action.payload);
+      return state.updateIn(["councilmeetinglist"], list => list.concat(fromJS(action.payload)));
     default:
-    return state;
+      return state;
   }
 }
--- test/councilmeeting/councilmeeting.reducer.spec.js
+++ test/councilmeeting/councilmeeting.reducer.spec.js
@@ -1,69 +1,61 @@
 import { expect } from "chai";
 import { fromJS } from "immutable";
 import reducer from "../../src/councilmeeting/councilmeeting.reducer";
-import { COUNCILMEETING_SAVE_ONE_SUCCESS } from "../../src/councilmeeting/councilmeeting.actions";
+import {
+  // COUNCILMEETING_SAVE_ONE_SUCCESS,
+  COUNCILMEETING_GET_ALL_SUCCESS,
+  // COUNCILMEETING_GET_ALL_FAILURE,
+}
+from "../../src/councilmeeting/councilmeeting.actions";
+
+
+const initialState = fromJS({
+  councilmeetinglist: [
+    {
+      id: 1,
+      date: "date1",
+    },
+    {
+      id: 2,
+      date: "date2",
+    },
+    {
+      id: 3,
+      date: "date3",
+    },
+  ],
+});
 
 describe("councilmeeting reducer", () => {
   it("should return the initial state", () => {
     expect(
       reducer(undefined, {})
-      ).to.equal(fromJS({
-        councilmeetinglist: [
-          {
-            id: 1,
-            date: "date1",
-          },
-          {
-            id: 2,
-            date: "date2",
-          },
-          {
-            id: 3,
-            date: "date3",
-          },
-        ],
-      }));
+      ).to.equal(initialState);
   });
-    it("should handle COUNCILMEETING_SAVE_ONE_SUCCESS", () => {
-      expect(
-        reducer([], {
-          type: COUNCILMEETING_SAVE_ONE_SUCCESS,
-          date: "date1"
-        })
-        ).toEqual([
-        {
-          id: 2,
-          date: "date1",
-        }
-        ])
 
-   /* expect(
-      reducer(
-        [
-          {
-            text: "Use Redux",
-            completed: false,
-            id: 0
-          }
-        ],
-        {
-          type: types.ADD_TODO,
-          text: 'Run the tests'
-        }
-      )
-    ).toEqual(
-      [
-        {
-          text: "Run the tests",
-          completed: false,
-          id: 1
-        },
-        {
-          text: "Use Redux",
-          completed: false,
-          id: 0
-        }
-      ]
-      )*/
-    })
-  })
+
+  it("should change meetinglist when receiving dates", () => {
+    const mockDatesFromAPI = [
+      {
+        id: 1,
+        date: "date1",
+      },
+      {
+        id: 2,
+        date: "date2",
+      },
+      {
+        id: 3,
+        date: "date3",
+      },
+    ];
+    const expectedState = initialState.updateIn(["councilmeetinglist"],
+      list => list.concat(fromJS(mockDatesFromAPI))
+      );
+    const newState = reducer(initialState, {
+      type: COUNCILMEETING_GET_ALL_SUCCESS,
+      payload: mockDatesFromAPI,
+    });
+    expect(newState).to.equal(expectedState);
+  });
+});
