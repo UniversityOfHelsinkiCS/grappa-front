ab0ff4cb1f5b18e56f20e2944ebebf6486efb26c
iorena <essi.salmenkivi@helsinki.fi>
2016-04-07 19:01:52 +0300

    add form validation

--- test/thesis/ThesisCreate.smart.spec.jsx
+++ test/thesis/ThesisCreate.smart.spec.jsx
@@ -14,12 +14,12 @@ import ThesisCreateContainer from "../../src/thesis/ThesisCreate.smart";
 import { saveThesis } from "../../src/thesis/thesis.actions";
 
 describe("ThesisCreate.smart", () => {
+  const component = renderIntoDocument(
+    <Provider store={store}>
+      <ThesisCreateContainer />
+    </Provider>
+  );
   it("should render the page", () => {
-    const component = renderIntoDocument(
-      <Provider store={store}>
-        <ThesisCreateContainer />
-      </Provider>
-    );
     const title = scryRenderedDOMComponentsWithClass(component, "ui dividing header");
     expect(title.length).to.equal(4);
     expect(title[0].textContent).to.equal("Made by");
@@ -38,5 +38,12 @@ describe("ThesisCreate.smart", () => {
     setTimeout(() => {
       expect(spy.calledOnce).to.equal(true);
     }, 1);
+  });
+
+  describe("ThesisCreate form validation", () => {
+    const button = scryRenderedDOMComponentsWithClass(component, "ui primary button")[1];
+    it("should keep submit button locked until all fields are filled", () => {
+      expect(button.disabled);
+    });
   });
 });
