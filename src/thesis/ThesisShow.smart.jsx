067ae1a2f436886519f4ff96f944a140bf423a6b
xvax <jesse.vaisanen@cs.helsinki.fi>
2016-04-26 21:46:30 +0300

    Lint

--- src/thesis/ThesisShow.smart.jsx
+++ src/thesis/ThesisShow.smart.jsx
@@ -5,6 +5,7 @@ export class ThesisShow extends Component {
   constructor() {
     super();
     this.handleEdit = this.handleEdit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
     this.state = {
       thesis: "",
     };
@@ -35,98 +36,131 @@ export class ThesisShow extends Component {
       }
     }
 
-  handleEdit() {
-    document.getElementsByTagName("textarea")[1].removeAttribute("readOnly");
-  }
+    handleChange(event) {
+      this.setState({ title: event.target.value });
+    }
 
-  handleSave() {
-    document.getElementsByTagName("textarea")[1].readOnly = true;
-  }
+    handleEdit() {
+      document.getElementsByTagName("textarea")[1].removeAttribute("readOnly");
 
-  renderContent() {
-    const thesis = this.state.thesis;
-    return (
-      <div>
-        <h3 className="ui dividing header">{ thesis.title }</h3>
+
+      for (let i = 0; i < 5; i++) {
+        document.getElementsByTagName("input")[i].removeAttribute("readOnly");
+      }
+    }
+
+    handleSave() {
+      document.getElementsByTagName("textarea")[1].readOnly = true;
+      for (let i = 0; i < 5; i++) {
+        document.getElementsByTagName("input")[i].readOnly = true;
+      }
+    }
+
+
+    renderContent() {
+      const thesis = this.state.thesis;
+      let ethesis = "Missing";
+      let evalGraders = "Not Done";
+      let docSent = "Not Sent";
+
+      if (thesis.ethesis !== "" && thesis.ethesis !== null) {
+        ethesis = "Added";
+      }
+      if (thesis.ThesisProgress.gradersStatus) {
+        evalGraders = "Done";
+      }
+      if (thesis.ThesisProgress.documentsSent !== null && thesis.ThesisProgress.documentsSent !== "") {
+        docSent = "Sent";
+      }
+      return (
         <div>
+          <div>
+            <form className="ui form">
+              <div className="field">
+                <div>Title:</div>
+                <input type="text" readOnly value={thesis.title} />
+                <div>Name:</div>
+                <input type="text" readOnly value={ thesis.author } />
+                <div>Grade:</div>
+                <input type="text" readOnly value={ thesis.grade } />
+              </div>
+              <div className="field">
+                <div>Ethesis link:</div>
+                <input type="text" readOnly value={ thesis.ethesis } />
+              </div>
+              <div className="field">
+                <div>Urkund link:</div>
+                <input type="text" readOnly value={ thesis.urkund } />
+              </div>
+              <h4 className="ui dividing header">Abstract</h4>
+              <textarea className="abstract" value={ thesis.abstract } readOnly rows="5" cols="30" />
+              <div className="field">
+                <h4 className="ui dividing header">Graders</h4>
+                <div className="field">
+                  <h5>Grader 1</h5>
+                  <div>Grader name: { thesis.Graders[0].name }</div>
+                  <div>Grader title: { thesis.Graders[0].title }</div>
+                </div>
+                <div className="field">
+                  <h5>Grader 2</h5>
+                  <div>Grader name: { thesis.Graders[1].name }</div>
+                  <div>Grader title: { thesis.Graders[1].title }</div>
+                </div>
+              </div>
+              <h4 className="ui dividing header">Grader Evaluation</h4>
+              <textarea className="eval" readOnly rows="5" cols="30" />
+            </form>
+          </div>
           <form className="ui form">
-            <div className="field">
-              <div>{ thesis.author }</div>
-              <div>Lol</div>
-              <div>{ thesis.grade }</div>
+            <h4 className="ui dividing header">Notifications</h4>
+            <div className="three fields">
+              <div className="field">Name and status</div>
+              <div className="field">Date when last notification sent</div>
+              <div className="field">Buttons for sending notifications</div>
             </div>
-            <div className="field">
-              <div className="field">{ thesis.ethesis }</div>
-            </div>
-            <div className="field">{ thesis.urkund }</div>
-            <h4 className="ui dividing header">Abstract</h4>
-            <textarea className="abstract" value={ thesis.abstract } readOnly rows="5" cols="30" />
-            <div className="field">
-              <h4 className="ui dividing header">Graders</h4>
+            <div className="three fields">
               <div className="field">
-                <div>Simo Häyhä</div>
-                <div>PhD</div>
+                Ethesis Link
+                <div className="ui tag label"> { ethesis } </div>
               </div>
               <div className="field">
-                <div>Jari Kurri</div>
-                <div>PROFESSOR</div>
+                <p>{ thesis.ThesisProgress.ethesisReminder }</p>
+              </div>
+              <div className="field">
+                <button className="ui blue tiny button">Send notification</button>
               </div>
             </div>
-            <h4 className="ui dividing header">Grader Evaluation</h4>
-            <textarea className="eval" readOnly rows="5" cols="30" />
+            <div className="three fields">
+              <div className="field">
+                Grader Evaluation
+                <div className="ui tag label"> { evalGraders } </div>
+              </div>
+              <div className="field">
+                <p>{ thesis.ThesisProgress.professorReminder }</p>
+              </div>
+              <div className="field">
+                <button className="ui blue tiny button">Send notification</button>
+              </div>
+            </div>
+            <div className="three fields">
+              <div className="field">
+                Documents
+                <div className="ui tag label"> { docSent } </div>
+              </div>
+              <div className="field">
+                <p>{ thesis.ThesisProgress.documentsSent }</p>
+              </div>
+              <div className="field">
+                <button className="ui blue tiny button">Resend</button>
+              </div>
+            </div>
           </form>
+          <h3 id="deadlineReminder">Deadline: { thesis.deadline }</h3>
+          <button className="ui primary button" id="editButton" onClick={ this.handleEdit }>Edit</button>
+          <button className="ui primary button" id="saveButton" onClick={ this.handleSave }>Save</button>
         </div>
-        <form className="ui form">
-          <h4 className="ui dividing header">Notifications</h4>
-          <div className="three fields">
-            <div className="field">
-              <div className="ui checkbox">
-                <input type="checkbox" />
-                <label>E-thesis</label>
-              </div>
-            </div>
-            <div className="field">
-              <p>"latest notification sent, date"</p>
-            </div>
-            <div className="field">
-              <button className="ui blue tiny button">Send notification</button>
-            </div>
-          </div>
-          <div className="three fields">
-            <div className="field">
-              <div className="ui checkbox">
-                <input type="checkbox" />
-                <label>Reports</label>
-              </div>
-            </div>
-            <div className="field">
-              <p>"latest notification sent, date"</p>
-            </div>
-            <div className="field">
-              <button className="ui blue tiny button">Send notification</button>
-            </div>
-          </div>
-          <div className="three fields">
-            <div className="field">
-              <div className="ui checkbox">
-                <input type="checkbox" />
-                <label>Documents sent</label>
-              </div>
-            </div>
-            <div className="field">
-              <p>"date when sent"</p>
-            </div>
-            <div className="field">
-              <button className="ui blue tiny button">Resend</button>
-            </div>
-          </div>
-        </form>
-        <h3 id="deadlineReminder">Deadline: { thesis.deadline }</h3>
-        <button className="ui primary button" id="editButton" onClick={ this.handleEdit }>Edit</button>
-        <button className="ui primary button" id="saveButton" onClick={ this.handleSave }>Save</button>
-      </div>
-    );
-  }
+      );
+    }
 
   render() {
     // console.log("state is ")
