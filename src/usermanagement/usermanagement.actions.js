8bfc8019a69272a7b61216769a2d5ad8fd1cd5f4
Henri Piironen <hcpiiron@sf-10.cs.helsinki.fi>
2016-04-26 18:38:25 +0300

    fix bugs

--- src/usermanagement/usermanagement.actions.js
+++ src/usermanagement/usermanagement.actions.js
@@ -1,8 +1,22 @@
+/*
+* The file containing all the defined actions that one can call to gain access
+* to modifying the state in regard to listing, accept and decline users.
+*/
 import { CALL_API } from "../middleware/grappaAPI";
 
 export const USERS_GET_ALL_SUCCESS = "USERS_GET_ALL_SUCCESS";
 export const USERS_GET_ALL_FAILURE = "USERS_GET_ALL_FAILURE";
+export const USER_UPDATE_ONE_LOCAL = "USERS_GET_ALL_FAILURE";
+export const USER_UPDATE_ONE_SUCCESS = "USER_UPDATE_ONE_SUCCESS";
+export const USER_UPDATE_ONE_FAILURE = "USER_UPDATE_ONE_FAILURE";
+export const USER_DECLINE_SUCCESS = "USER_DECLINE_SUCCESS";
+export const USER_DECLINE_FAILURE = "USER_DECLINE_FAILURE";
 
+/*
+* The action called to get a list of all new users in the database.
+* @return getUsers The object containing the relevant information for the
+* reducer to handle the data accordingly.
+*/
 export const getUsers = () => {
   console.log("getUsers-action called!");
   return {
@@ -10,7 +24,39 @@ export const getUsers = () => {
     success: USERS_GET_ALL_SUCCESS,
     failure: USERS_GET_ALL_FAILURE,
     method: "get",
-    url: "/user",
+    url: "/user/new",
     data: {},
+  };
+};
+
+export const updateUserLocally = (user) => {
+  console.log("updateUser-action called!");
+  return {
+    type: USER_UPDATE_ONE_LOCAL,
+    data: user,
+  };
+};
+
+export const updateUser = (user) => {
+  console.log("updateUser-action called!");
+  return {
+    type: CALL_API,
+    success: USER_UPDATE_ONE_SUCCESS,
+    failure: USER_UPDATE_ONE_FAILURE,
+    method: "put",
+    url: "/user/",
+    data: user,
+  };
+};
+
+export const declineUser = (user) => {
+  console.log("declineUser-action called!");
+  return {
+    type: CALL_API,
+    success: USER_DECLINE_SUCCESS,
+    failure: USER_DECLINE_FAILURE,
+    method: "delete",
+    url: "/user/",
+    data: user,
   };
 };
--- src/usermanagement/usermanagement.reducer.js
+++ src/usermanagement/usermanagement.reducer.js
@@ -2,13 +2,14 @@ import { Map, fromJS } from "immutable";
 import {
   USERS_GET_ALL_SUCCESS,
   USERS_GET_ALL_FAILURE,
-
+  USER_UPDATE_ONE_SUCCESS,
+  USER_UPDATE_ONE_FAILURE,
+  USER_DECLINE_SUCCESS,
+  USER_DECLINE_FAILURE,
 } from "./usermanagement.actions";
 
 const INITIAL_STATE = fromJS({
-  users: [
-    {},
-  ],
+  users: [],
 });
 
 export default function (state = INITIAL_STATE, action) {
@@ -18,6 +19,16 @@ export default function (state = INITIAL_STATE, action) {
     case USERS_GET_ALL_FAILURE:
     // probably should display error message?
       return state;
+    case USER_UPDATE_ONE_SUCCESS:
+      return state.updateIn(["users"], list => list.push(new Map(action.payload)));
+    case USER_UPDATE_ONE_FAILURE:
+    // probably should display error message?
+    case USER_DECLINE_SUCCESS:
+      return state;
+    case USER_DECLINE_FAILURE:
+      return state;
+    // case USER_UPDATE_ONE_LOCAL:
+    //   return 
     default:
       return state;
   }
