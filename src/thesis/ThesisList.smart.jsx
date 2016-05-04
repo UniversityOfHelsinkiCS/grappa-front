0b635bf451ad7f671616c4acba66ac2f2c3fa4b2
iorena <essi.salmenkivi@helsinki.fi>
2016-05-04 11:46:18 +0300

    new table, works like a dream

--- src/thesis/ThesisList.smart.jsx
+++ src/thesis/ThesisList.smart.jsx
@@ -6,7 +6,7 @@
 */
 
 import React, { Component } from "react";
-import { BootstrapTable, TableHeaderColumn } from "react-bootstrap-table";
+import { Table, Thead, Th, unsafe } from "reactable";
 import { linkFormatter, studyFieldNameFormatter, dateFormatter, instructorFormatter, statusFormatter } from "../config/helpers";
 
 
@@ -15,49 +15,91 @@ export class ThesisList extends Component {
     super();
     this.linkFormatter = linkFormatter.bind(this);
     this.studyFieldNameFormatter = studyFieldNameFormatter.bind(this);
-    this.statuses = {
-      true: "Done",
-      false: "In progress",
-    };
     this.dateFormatter = dateFormatter.bind(this);
     this.instructorFormatter = instructorFormatter.bind(this);
     this.statusFormatter = statusFormatter.bind(this);
+    this.state = {};
+    this.state.theses = [];
+    this.state.allTheses = [];
+    this.state.inProgressTheses = [];
   }
 
   /*
   * Defines what is done at the beginning of the components life before rendering.
+  * Switches the state used by Table between all and in progress theses
   */
   componentWillMount() {
     const { getTheses } = this.props;
     getTheses();
+    this.selectFields(this.props.theses);
+    this.setState({ theses: this.state.inProgressTheses });
   }
 
   /*
+   * Selects desired fields from raw thesis objects and formats them
+   * We keep track of two sets of theses: all and only in progress
+   */
+  selectFields(theses) {
+    console.log(theses);
+    for (let i = 0; i < theses.length; i++) {
+      console.log(theses[i]);
+      const origDate = new Date(theses[i].deadline);
+      const newDate = `${origDate.getDate()}/${origDate.getMonth()}/${origDate.getFullYear()}`;
+      const link = '<a href="thesis/' + theses[i].id + '">' + theses[i].title + "</a>";
+      const newThesis = {
+        Status: theses[i].ThesisProgress.isDone ? "Done" : "In progress",
+        Author: theses[i].author,
+        title: unsafe(link),
+        graders: theses[i].User.name,
+        StudyField: theses[i].StudyField.name,
+        deadline: newDate,
+      };
+      if (!theses[i].ThesisProgress.isDone) {
+        this.state.inProgressTheses.push(newThesis);
+      }
+      this.state.allTheses.push(newThesis);
+    }
+  }
+
+   /*
+   * Defines what happens when we check the checkbox
+   */
+  filter() {
+    if (this.refs.checkbox.checked) {
+      this.setState({ theses: this.state.allTheses });
+    } else {
+      this.setState({ theses: this.state.inProgressTheses });
+    }
+  }
+  /*
   * The method in charge of rendering the outlook of the page. Contains all the html elements.
-  * Contains a react-bootstrap-table library styled table.
+  * Contains a reactable library styled table.
   * @return <div>-container Container wrapping all the html elements to be rendered.
   */
 
   render() {
-    const { theses } = this.props;
-    console.log(theses);
+    const columns = [
+      "Status",
+      "Author",
+      "title",
+      "graders",
+      "StudyField",
+      "deadline",
+    ];
     return (
       <div>
         <h2>Theses</h2>
-        <BootstrapTable data={theses} search bordered={false}>
-          <TableHeaderColumn filter= {{ type: "TextFilter" }} dataField="id" isKey hidden>
-          Thesis ID</TableHeaderColumn>
-          <TableHeaderColumn dataField="isDone" dataFormat={ this.statusFormatter } formatExtraData={ this.statuses } >Status</TableHeaderColumn>
-          <TableHeaderColumn dataField="author" dataSort width="200">Author</TableHeaderColumn>
-          <TableHeaderColumn dataField="title" dataFormat={ this.linkFormatter } dataSort width="200">Thesis Title</TableHeaderColumn>
-          <TableHeaderColumn dataFormat={ this.instructorFormatter } dataSort width="200">Instructor
-          </TableHeaderColumn>
-          <TableHeaderColumn dataField="email" dataSort width="200">Email</TableHeaderColumn>
-          <TableHeaderColumn dataFormat={ this.studyFieldNameFormatter } dataSort width="200">Field
-          </TableHeaderColumn>
-          <TableHeaderColumn dataFormat={ this.dateFormatter } dataSort width="200">Deadline
-          </TableHeaderColumn>
-        </BootstrapTable>
+        Show also finished theses<input ref="checkbox" className="checkbox" type="checkbox" onClick={this.filter.bind(this)}/>
+        <Table noDataText="No theses found" className="table" ref="table" sortable columns={columns} data={this.state.theses} filterable={columns}>
+          <Thead>
+            <Th column="Status">Status</Th>
+            <Th column="Author">Author</Th>
+            <Th column="title">Thesis title</Th>
+            <Th column="graders">Instructor</Th>
+            <Th column="StudyField">Field</Th>
+            <Th column="deadline">Deadline</Th>
+          </Thead>
+        </Table>
       </div>
       );
   }
