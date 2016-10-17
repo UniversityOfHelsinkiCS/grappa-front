147e9a4bdc717de89e625fba3441855f9a450e1a
TeemuKoivisto <tkoivisto456@gmail.com>
2016-10-17 15:07:05 +0300

    Fix Ethesis actions

--- src/ethesis/ethesis.actions.js
+++ src/ethesis/ethesis.actions.js
@@ -1,29 +1,28 @@
 
-import { CALL_API } from "../middleware/grappaAPI";
-
-export const ETHESIS_SENT_SUCCESS = "ETHESIS_SENT_SUCCESS";
-export const ETHESIS_SENT_FAILURE = "ETHESIS_SENT_FAILURE";
+export const ETHESIS_SENT = "ETHESIS_SENT";
 
 export const updateThesisesEthesis = (token, link) => (
   {
-    type: CALL_API,
+    type: ETHESIS_SENT,
     flashMessage: {
       type: "warning",
       title: "Request sent",
       body: "Waiting for ethesis link to be saved.",
     },
-    success: ETHESIS_SENT_SUCCESS,
     successMessage: {
       type: "success",
       title: "Success",
       body: "Link has been saved.",
     },
-    failure: ETHESIS_SENT_FAILURE,
-    method: "post",
-    url: "/thesis/ethesis",
-    data: {
-      token,
-      link,
+    payload: {
+      request: {
+        method: "post",
+        url: "/thesis/ethesis",
+        data: {
+          token,
+          link,
+        }
+      }
     }
   }
 );
