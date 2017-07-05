b34653b6cb095fbc25a35bba8b4606eef46ef60a
Jakousa <jakousa@cs.helsinki.fi>
2017-07-05 13:43:37 +0300

    Got grades from backend

--- src/thesis/StatisticsPage.jsx
+++ src/thesis/StatisticsPage.jsx
@@ -3,11 +3,27 @@ import React, { Component } from "react";
 import StatisticsTable from "./statistics/StatisticsTable";
 
 export class StatisticsPage extends Component { 
+
+    constructor(props) {
+        super(props);
+    }
+
+    componentWillMount() {
+        this.props.getGrades();
+    }
+
     render() {
+        const grades = [1,2,3,4,5];
+        const grades2 = ['A','B','C','D','E'];
+        const programmes = ["??", "??", "??"];
+        const programmes2 = ["Software Systems", "Networking and Services", "Algorithms, Data Analytics and Machine Learning", "Algorithmic Bioinformatics"];
+        const grades3 = [];
+        const programmes3 = [];
         return (
             <div>
-            <StatisticsTable />
-            <StatisticsTable />
+            <StatisticsTable columnDefinition={grades} rowDefinition={programmes}/>
+            <StatisticsTable columnDefinition={grades2} rowDefinition={programmes2}/>
+            <StatisticsTable columnDefinition={grades3} rowDefinition={programmes3}/>
             </div>
         );
     }
@@ -20,4 +36,12 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps)(StatisticsPage);
+import { getGrades } from "../thesis/thesis.actions";
+
+const mapDispatchToProps = (dispatch) => ({
+    getGrades(){
+        dispatch(getGrades());
+    },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(StatisticsPage);
--- src/thesis/statistics/StatisticsTable.jsx
+++ src/thesis/statistics/StatisticsTable.jsx
@@ -2,10 +2,39 @@ import React, { Component } from "react";
 
 export class StatisticsTable extends Component {
     render() {
+        if (this.props.rowDefinition.length <= 0 || this.props.columnDefinition.length <= 0) {
+            return (<div/>);
+        }
         return (
-            <div>
-            <p>Statistics</p>
-            </div>
+            <table className="ui fixed definition table">
+                <thead>
+                    <tr>
+                        <th></th>
+                        {this.props.columnDefinition.map((value, key) =>
+                            <th key={key}>{value}</th>
+                        )}
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.props.rowDefinition.map((value, key) =>
+                    <tr key={key}>
+                        <td>{value}</td>
+                        <td>None</td>
+                        <td>None</td>
+                        <td>None</td>
+                        <td>None</td>
+                        <td>None</td>
+                    </tr>
+                    )}
+                    <tr>
+                        <td>Sum</td>
+                        <td>None</td>
+                        <td>None</td>
+                        <td>None</td>
+                        <td>None</td>
+                        <td>None</td>
+                    </tr>
+                </tbody></table>
         );
     }
 }
@@ -13,8 +42,8 @@ export class StatisticsTable extends Component {
 import { connect } from "react-redux";
 
 const mapStateToProps = (state) => {
-  return {
-  };
+    return {
+    };
 };
 
 export default connect(mapStateToProps)(StatisticsTable);
