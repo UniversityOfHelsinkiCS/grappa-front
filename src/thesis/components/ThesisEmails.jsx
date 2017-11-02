643f8ace887158edf8e05453ac8fe404bbbf52d7
Jakousa <jakousa@cs.helsinki.fi>
2017-11-02 10:36:20 +0200

    resend notifications

--- src/thesis/components/ThesisEmails.jsx
+++ src/thesis/components/ThesisEmails.jsx
@@ -56,6 +56,38 @@ export class ThesisEmails extends Component {
         );
     }
 
+    renderNotification(name, type, reminder, isDone) {
+        return (
+            <div className="field">
+                <div className="ui right input">
+                    <h3 className="ui header">{name}</h3>
+                    <div className="ui checkbox m-left">
+                        <input type="checkbox" readOnly="true" checked={isDone ? "true" : ""} />
+                        <label></label>
+                    </div>
+                </div>
+                <div className="three fields">
+                    <div className="field">
+                        <label>Recipient</label>
+                        <p>{reminder.to}</p>
+                    </div>
+                    <div className="field">
+                        <label>Last sent</label>
+                        {reminder.lastSent ? <p>{moment(new Date(reminder.lastSent)).format("DD/MM/YYYY HH:mm")}</p> : <p></p>}
+                    </div>
+                        <div>
+                            <div className="field">
+                                <label>&nbsp;</label>
+                                <button className="ui blue button" onClick={() => this.sendReminder(type)}>
+                                    Send notification again
+                                </button>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        );
+    }
+
     render() {
         const thesisProgress = this.props.thesisProgress;
         return (
@@ -64,8 +96,8 @@ export class ThesisEmails extends Component {
                 {this.renderReminder("Ethesis Reminder", "EthesisReminder", thesisProgress.EthesisReminder || {}, thesisProgress.ethesisDone)}
                 {this.renderReminder("Grader Evaluation Reminder", "GraderEvalReminder", thesisProgress.GraderEvalReminder || {}, thesisProgress.graderEvalDone)}
                 {this.renderReminder("Print Thesis Reminder", "PrintReminder", thesisProgress.PrintReminder || {}, thesisProgress.printDone)}
-                {this.renderReminder("Student Notification", "StudentRegistrationNotification", thesisProgress.StudentRegistrationNotification || {}, thesisProgress.studentNotificationSent || thesisProgress.StudentRegistrationNotification != null)}
-                {this.renderReminder("Supervising Professor Notification", "SupervisingProfessorNotification", thesisProgress.SupervisingProfessorNotification || {}, thesisProgress.SupervisingProfessorNotification != null)}
+                {this.renderNotification("Student Notification", "StudentRegistrationNotification", thesisProgress.StudentRegistrationNotification || {}, thesisProgress.studentNotificationSent || thesisProgress.StudentRegistrationNotification != null)}
+                {this.renderNotification("Supervising Professor Notification", "SupervisingProfessorNotification", thesisProgress.SupervisingProfessorNotification || {}, thesisProgress.SupervisingProfessorNotification != null)}
             </div>
         );
     }
